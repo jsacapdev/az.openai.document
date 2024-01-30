9940b998080317381f87e8c7435ac11b8bee47b1
Jas Atwal <jas.atwal@capgemini.com>
2024-01-30 16:20:03 +0000

    ecde4deb-2032-4358-acae-d96204c7432b

--- scripts/blob_update.py
+++ scripts/blob_update.py
@@ -1,28 +1,43 @@
 import asyncio
-# import os
+import os
 
-# from azure.identity.aio import AzureDeveloperCliCredential
-
-# from auth_common import (
-#     get_application,
-#     get_auth_headers,
-#     test_authentication_enabled,
-#     update_application,
-# )
-
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.authorization import AuthorizationManagementClient
+from azure.mgmt.resource import ResourceManagementClient
+from dotenv import load_dotenv
+from pathlib import Path
 
 async def main():
-    # if not test_authentication_enabled():
-    #     print("Not updating authentication.")
-    #     exit(0)
 
-    print("This line will be printed.")
+    dotenv_path = Path('./.azure/dev/.env')
+    load_dotenv(dotenv_path=dotenv_path)
 
-    # credential = AzureDeveloperCliCredential(tenant_id=os.getenv("AZURE_AUTH_TENANT_ID", os.getenv("AZURE_TENANT_ID")))
+    AZURE_SUBSCRIPTION_ID = os.environ.get("AZURE_SUBSCRIPTION_ID", None)
+    GROUP_NAME = "rg-azd1-dev-001"
 
-    # client_app_id = os.getenv("AZURE_CLIENT_APP_ID", None)
-    # if client_app_id:
-    #     print(f"Application update for client app id {client_app_id} complete.")
+    resource_client = ResourceManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id=AZURE_SUBSCRIPTION_ID
+    )
+    authorization_client = AuthorizationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id=AZURE_SUBSCRIPTION_ID
+    )
+
+    resource_group = resource_client.resource_groups.get(
+        GROUP_NAME
+    )
+
+    # Get "Contributor" built-in role as a RoleDefinition object
+    role_name = 'Contributor'
+    roles = list(authorization_client.role_definitions.list(
+        resource_group.id,
+        filter="roleName eq '{}'".format(role_name)
+    ))
+    assert len(roles) == 1
+    contributor_role = roles[0]
+
+    print("!!!")
 
 if __name__ == "__main__":
     asyncio.run(main())
